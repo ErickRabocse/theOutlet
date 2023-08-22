5855a529903abe9cf8579b62d387d3f2026c5c45
Erick Chávez Escobar <erickchavezescobar@192.168.1.5>
2023-08-22 14:02:10 -0600

    fetch hook and styles added to home page

---
+++ src/Hooks/useFetch.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+
+const useFetch = (url) => {
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+  const [data, setData] = useState(undefined)
+
+  useEffect(() => {
+    setLoading(true)
+    setError(false)
+    setData(undefined)
+
+    //   CRETING AN ABORT controller
+    const controller = new AbortController()
+
+    //   CREATING AN ABORT signal
+    fetch(url, { signal: controller.signal })
+      .then(res => {
+        //  CHECKING STATUS ENSURES EVERYTHING WAS succussful
+        if (res.status === 200) {
+          return res.json()
+        }
+        // Promise.reject RETURNS an ERROR if something failed
+        return Promise.reject(res)
+      })
+      .then(setData)
+      .catch((e) => {
+        if (e.name === 'AbortError') return
+        setError(true)
+      })
+      .finally(() => {
+        // KEEPING LOADING ON SCREEN IF site reloads
+        if (controller.signal.aborted) return
+        setLoading(false)
+      })
+    //   NOTHING WILL RUN if a new fetch request is done!
+    return () => {
+      controller.abort()
+    }
+  }, [url])
+
+  return (
+    { loading, error, data }
+  )
+}
+export default useFetch
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,35 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import useFetch from '../Hooks/useFetch'
+import '@/styles/home.scss'
 
 const Home = () => {
-  const [items, setItems] = useState([])
-
-  useEffect(() => {
-    fetch('https://theoutlet.onrender.com/items')
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        setItems(data)
-      })
-  }, [])
+  const BASE_URL = 'https://theoutlet.onrender.com/items'
+  const [url] = useState(BASE_URL)
+  const { loading, error, data } = useFetch(url)
   return (
     <>
-      {
-      items.map(item => (
-        <div key={item.key}>{item.description}</div>
-      ))
-    }
+      {loading && <h3>Loading...</h3>}
+      {error && <p>There is a problem with the server please email us at: <span style={{ color: 'blue' }}>costumer_service@theoutlet.com</span></p>}
+      {data &&
+        <div className='container'>
+          <div className='row'>
+            {
+          data.map(item => (
+            <div key={item.id} className='col-sm-3 mb-3 mb-sm-0'>
+              <div className='card'>
+                <img className='card-img-top' src={item.image} alt={item.description} style={{ height: '300px', width: '100%', objectFit: 'contain' }} />
+                <div className='card-body'>
+                  <h5 className='card-title'>{item.product_name}</h5>
+                  <p className='card-text'> ${item.price}</p>
+                  <a href='#' className='btn btn-primary'>See details</a>
+                </div>
+              </div>
+            </div>
+          ))
+        }
+          </div>
 
+        </div>}
     </>
   )
 }
